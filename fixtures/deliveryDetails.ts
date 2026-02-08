428b97cddfe80445848846e00b8837bb9ef89a6e
Reiner Tolentino <reiner.tolentino@gmail.com>
2026-02-08 21:47:29 +0800

    added data in fixture

---
+++ fixtures/deliveryDetails.ts
@@ -0,0 +1,11 @@
+export const deliveryDetails:DeliveryDetails = {
+    firstName: "Reiner",
+    lastName: "Tolentino",
+    postCode: "1234",
+}
+
+export interface DeliveryDetails {
+    firstName: string,
+    lastName: string,
+    postCode: string
+}
